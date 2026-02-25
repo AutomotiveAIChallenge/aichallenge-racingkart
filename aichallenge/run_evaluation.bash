e23a18386edbd2d0e33888506eeda04fe549f4c2
taikitanaka3 <taiki.tanaka@tier4.jp>
2026-02-26 00:15:05 +0900

    refactor: rename OUTPUT_RUN_DIR to LOG_DIR

--- aichallenge/run_evaluation.bash
+++ aichallenge/run_evaluation.bash
@@ -25,7 +25,7 @@ rosbag="${AIC_ROSBAG:-true}"
 exec ros2 launch aichallenge_system_launch evaluation.launch.xml \
     "domain_id:=${domain_id}" \
     "sim_mode:=${sim_mode}" \
-    "output_run_dir:=${out_dir}" \
+    "log_dir:=${out_dir}" \
     "capture:=${capture}" \
     "rosbag:=${rosbag}" \
     "simulation:=true" \
