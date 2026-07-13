e322ff0edd9f730652613acd7357580baefd4eea
norikenpi <norikenpi@gmail.com>
2026-07-13 15:55:14 +0900

    feat: integrate recovery supervisor with MPC

--- aichallenge/run_autoware.bash
+++ aichallenge/run_autoware.bash
@@ -12,6 +12,9 @@ case "${mode}" in
 "awsim-no-viz")
     opts=("simulation:=true" "use_sim_time:=true" "run_rviz:=false")
     ;;
+"awsim-mpc-recovery")
+    opts=("simulation:=true" "use_sim_time:=true" "run_rviz:=false" "control_method:=mpc_recovery")
+    ;;
 "vehicle")
     opts=("simulation:=false" "use_sim_time:=false" "run_rviz:=false")
     ;;
@@ -19,7 +22,7 @@ case "${mode}" in
     opts=("simulation:=false" "use_sim_time:=true" "run_rviz:=true")
     ;;
 *)
-    echo "invalid argument (use 'awsim' or 'vehicle' or 'rosbag')"
+    echo "invalid argument (use 'awsim', 'awsim-no-viz', 'awsim-mpc-recovery', 'vehicle', or 'rosbag')"
     exit 1
     ;;
 esac
