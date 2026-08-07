da9e538f05b6ee4bc68b77a2b8be369bdd71cf4e
Masahiro Kubota <42679530+masahiro-kubota@users.noreply.github.com>
2026-08-07 18:11:05 +0900

    Update multiplay-client.sh

--- aichallenge/simulator_scripts/multiplay-client.sh
+++ aichallenge/simulator_scripts/multiplay-client.sh
@@ -4,10 +4,16 @@ AWSIM_DIRECTORY=/aichallenge/simulator/AWSIM
 export ROS_DOMAIN_ID=0
 
 exec $AWSIM_DIRECTORY/AWSIM.x86_64 \
+    --collisions off \
+    --start-mode count \
+    --start-count-seconds 0 \
+    --wall-recovery off \
+    --laps unlimited \
+    --timeout 10000000.0 \
     --multiplay client \
-    --multiplay-address 127.0.0.1 \
+    --multiplay-address 10.0.0.1 \
     --multiplay-port 7777 \
-    --multiplay-vehicle-index 1 \
-    --handicap off \
-    --ranking off \
-    --vehicles 1
+    --sound off \
+    --camera cpu \
+    --lidar cpu \
+    --multiplay-name <あなたの名前>
